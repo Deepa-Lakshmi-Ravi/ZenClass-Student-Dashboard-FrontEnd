a4b014a017f87d18ce61e4a9a50e9c5ebfb97bc1
Deepa-Lakshmi-Ravi <deviravichandren02@gmail.com>
2024-04-12 14:07:02 -0700

    added changes

--- src/Pages/Reset/ResetPassword.jsx
+++ src/Pages/Reset/ResetPassword.jsx
@@ -42,13 +42,16 @@ const ResetPasswordForm = () => {
         }, 2000);
       }
     } catch (error) {
-      console.log(error);
-      toast.error(
-        "Invalid token or token has expired.Please request a new reset link.",
-        {
-          position: "top-center",
-        }
-      );
+      if (error.response && error.response.status === 400) {
+        toast.error(
+          "Invalid token or token has expired.Please request a new reset link.",
+          {
+            position: "top-center",
+          }
+        );
+      } else {
+        console.log(error);
+      }
     } finally {
       setLoading(false);
     }
