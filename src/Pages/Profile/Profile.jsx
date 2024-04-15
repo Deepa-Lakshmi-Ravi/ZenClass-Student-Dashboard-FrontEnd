ede2eb0927b6e5c0e9db3d961875885d04d9fcc2
Deepa-Lakshmi-Ravi <deviravichandren02@gmail.com>
2024-04-15 07:59:47 -0700

    some code canges in profile

--- src/Pages/Profile/Profile.jsx
+++ src/Pages/Profile/Profile.jsx
@@ -1,88 +1,96 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import "./Profile.css";
 import DataContext from "../../Context/dataContext";
 import { ToastContainer } from "react-toastify";
-import { Formik, Form, Field } from "formik";
-import * as Yup from "yup";
+//import * as Yup from "yup";
 
-const Validate = Yup.object().shape({
-  firstName: Yup.string()
-    .min(6, "Must be atleast 6 characters")
-    .max(30, "Must be less than 30 characters")
-    .required("Required"),
-  lastName: Yup.string()
-    .min(6, "Must be atleast 6 characters")
-    .max(30, "Must be less than 30 characters")
-    .required("Required"),
-  email: Yup.string().email("Invalid email").required("Required"),
-  contactNo: Yup.string()
-    .min(10, "Must be atleast 10 characters")
-    .max(15, "Must be less than 15 characters")
-    .required("Required"),
-  batch: Yup.string().required("Required"),
-  qualification: Yup.string()
-    .min(1, "Must be atleast 1 character")
-    .max(10, "Must be less than 10 characters")
-    .required("Required"),
-  experience: Yup.string()
-    .min(1, "Must be atleast 1 character")
-    .max(35, "Must be less than 35 characters")
-    .required("Required"),
-  yearofpassing: Yup.string()
-    .min(1, "Must be atleast 1 character")
-    .max(35, "Must be less than 35 characters")
-    .required("Required"),
-  noticeperiod: Yup.string().required(true),
-  portfolioUrl: Yup.string().required(true),
-  githubUrl: Yup.string().required(true),
-  resumeUrl: Yup.string().required(true),
-});
+// const Validate = Yup.object().shape({
+//   firstName: Yup.string()
+//     .min(6, "Must be atleast 6 characters")
+//     .max(30, "Must be less than 30 characters")
+//     .required("Required"),
+//   lastName: Yup.string()
+//     .min(6, "Must be atleast 6 characters")
+//     .max(30, "Must be less than 30 characters")
+//     .required("Required"),
+//   email: Yup.string().email("Invalid email").required("Required"),
+//   contactNo: Yup.string()
+//     .min(10, "Must be atleast 10 characters")
+//     .max(15, "Must be less than 15 characters")
+//     .required("Required"),
+//   batch: Yup.string().required("Required"),
+//   qualification: Yup.string()
+//     .min(1, "Must be atleast 1 character")
+//     .max(10, "Must be less than 10 characters")
+//     .required("Required"),
+//   experience: Yup.string()
+//     .min(1, "Must be atleast 1 character")
+//     .max(35, "Must be less than 35 characters")
+//     .required("Required"),
+//   yearofpassing: Yup.string()
+//     .min(1, "Must be atleast 1 character")
+//     .max(35, "Must be less than 35 characters")
+//     .required("Required"),
+//   noticeperiod: Yup.string().required(true),
+//   portfolioUrl: Yup.string().required(true),
+//   githubUrl: Yup.string().required(true),
+//   resumeUrl: Yup.string().required(true),
+// });
 
 const Profile = () => {
-  const { loggedUser, loading, handleHead, handleProfileUpdate } =
-    useContext(DataContext);
+  const { loggedUser, loading, handleHead, handleProfileUpdate } = useContext(DataContext);
 
   useEffect(() => {
     handleHead("Update Profile");
   }, []);
 
+  const [formValues, setFormValues] = useState({
+    firstName: loggedUser.firstName,
+    lastName: loggedUser.lastName,
+    email: loggedUser.email,
+    contactNo: loggedUser.contactNo,
+    qualification: loggedUser.qualification,
+    experience: loggedUser.experience,
+    yearofpassing: "",
+    noticePeriod: "",
+    githubUrl: "",
+    portfolioUrl: "",
+    resumeUrl: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues((prevValues) => ({
+      ...prevValues,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    handleProfileUpdate(formValues);
+  };
+
+
   return (
       <section className="profile">
         <div className="container mt-5">
-          <Formik
-            initialValues={{
-              firstName: loggedUser.firstName,
-              lastName: loggedUser.lastName,
-              email: loggedUser.email,
-              contactNo: loggedUser.contactNo,
-              qualification: loggedUser.qualification,
-              experience: loggedUser.experience,
-              yearofpassing: "",
-              noticePeriod: "",
-              githubUrl: "",
-              portfolioUrl: "",
-              resumeUrl: "",
-            }}
-            validationSchema={Validate}
-            onSubmit={(values, { resetForm }) => {
-              handleProfileUpdate(values);
-              resetForm({values:"" });
-            }}
-          >
             
-            <Form className="form-cards">
+            <form onSubmit={handleSubmit} className="form-cards">
               <div className="detailsCard">
                 <div className="personalDetails">
                   <div className="form-group">
                     <label className="label-style" htmlFor="firstName">
                       FirstName
                     </label>
-                    <Field
+                    <input
                       type="text"
                       name="firstName"
                       id="firstName"
                       placeholder="Eg: John"
                       className="form-control"
+                      value={formValues.firstName}
+                      onChange={handleChange}
                     />
                   </div>
                   <br />
@@ -90,12 +98,14 @@ const Profile = () => {
                     <label className="label-style" htmlFor="lastName">
                       LastName
                     </label>
-                    <Field
+                    <input
                       type="text"
                       name="lastName"
                       id="lastName"
                       placeholder="Smith"
                       className="form-control"
+                      value={formValues.lastName}
+                      onChange={handleChange}
                     />
                   </div>
                   <br />
@@ -103,13 +113,15 @@ const Profile = () => {
                     <label className="label-style" htmlFor="email">
                       Email
                     </label>
-                    <Field
+                    <input
                       type="email"
                       name="email"
                       id="email"
                       placeholder="Eg: johnsmith@abc.com"
                       className="form-control"
                       disabled
+                      value={formValues.email}
+                      onChange={handleChange}
                     />
                   </div>
                   <br />
@@ -117,12 +129,14 @@ const Profile = () => {
                     <label className="label-style" htmlFor="contactNo">
                       Phone
                     </label>
-                    <Field
+                    <input
                       type="phone"
                       name="contactNo"
                       id="contactNo"
                       placeholder="xxxxxxxxxxxx"
                       className="form-control"
+                      value={formValues.contactNo}
+                      onChange={handleChange}
                     />
                   </div>
                 </div>
@@ -133,12 +147,14 @@ const Profile = () => {
                     <label className="label-style" htmlFor="qualification">
                       Qualification
                     </label>
-                    <Field
+                    <input
                       type="text"
                       name="qualification"
                       id="qualification"
                       placeholder="Eg: B.Tech"
                       className="form-control"
+                      value={formValues.qualification}
+                      onChange={handleChange}
                     />
                   </div>
                   <br />
@@ -146,12 +162,14 @@ const Profile = () => {
                     <label className="label-style" htmlFor="yearofpassing">
                       Year of Passing
                     </label>
-                    <Field
+                    <input
                       type="text"
                       name="yearofpassing"
                       id="yearofpassing"
                       placeholder=""
                       className="form-control"
+                      value={formValues.yearofpassing}
+                      onChange={handleChange}
                     />
                   </div>
                   <br />
@@ -159,12 +177,14 @@ const Profile = () => {
                     <label className="label-style" htmlFor="experience">
                       Years of Experience
                     </label>
-                    <Field
+                    <input
                       type="text"
                       name="experience"
                       id="experience"
                       placeholder="Eg: fresher"
                       className="form-control"
+                      value={formValues.experience}
+                      onChange={handleChange}
                     />
                   </div>
                   <br />
@@ -172,12 +192,14 @@ const Profile = () => {
                     <label className="label-style" htmlFor="noticeperiod">
                       Notice Period
                     </label>
-                    <Field
+                    <input
                       type="text"
                       name="noticePeriod"
                       id="noticePeriod"
                       placeholder="in days"
                       className="form-control"
+                      value={formValues.noticePeriod}
+                      onChange={handleChange}
                     />
                   </div>
                 </div>
@@ -188,12 +210,14 @@ const Profile = () => {
                     <label className="label-style" htmlFor="github">
                       Github URL
                     </label>
-                    <Field
+                    <input
                       type="text"
                       name="githubUrl"
                       id="githubUrl"
                       placeholder="Example: github.com/<Your-Username>"
                       className="form-control"
+                      value={formValues.githubUrl}
+                      onChange={handleChange}
                     />
                   </div>
                   <br />
@@ -201,12 +225,14 @@ const Profile = () => {
                     <label className="label-style" htmlFor="portfolio">
                       Portfolio URL
                     </label>
-                    <Field
+                    <input
                       type="text"
                       name="portfolioUrl"
                       id="portfolioUrl"
                       placeholder="Example: yourSite.com"
                       className="form-control"
+                      value={formValues.portfolioUrl}
+                      onChange={handleChange}
                     />
                   </div>
                   <br />
@@ -214,12 +240,14 @@ const Profile = () => {
                     <label className="label-style" htmlFor="resume">
                       Resume URL
                     </label>
-                    <Field
+                    <input
                       type="text"
                       name="resumeUrl"
                       id="resumeUrl"
                       placeholder="Example: docs.google.com/yourResumeParams"
                       className="form-control"
+                      value={formValues.resumeUrl}
+                      onChange={handleChange}
                     />
                   </div>
                 </div>
@@ -238,8 +266,7 @@ const Profile = () => {
                   </button>
                 </div>
               </div>
-            </Form>
-          </Formik>
+            </form>
         </div>
         <ToastContainer position="top-center" autoClose={3000} />
       </section>
