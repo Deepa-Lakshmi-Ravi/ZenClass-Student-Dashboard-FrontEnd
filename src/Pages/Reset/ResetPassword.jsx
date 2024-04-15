7703450f96f39195e1a84e80e19cfab2acb6459f
Deepa-Lakshmi-Ravi <deviravichandren02@gmail.com>
2024-04-14 22:04:01 -0700

    code change in reset password

--- src/Pages/Reset/ResetPassword.jsx
+++ src/Pages/Reset/ResetPassword.jsx
@@ -1,7 +1,8 @@
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import DataContext from "../../Context/dataContext";
-import { useParams, useNavigate } from "react-router-dom";
-import { ToastContainer, toast } from "react-toastify";
+import { useParams,useNavigate } from "react-router-dom";
+import { ToastContainer,toast } from "react-toastify";
+import { Formik, Form, Field } from "formik";
 import * as Yup from "yup";
 import logo from "../../assets/zen logo.png";
 import banner from "../../assets/zen banner.png";
@@ -10,37 +11,30 @@ import AxiosService from "../../Axios/AxiosService";
 
 const Validate = Yup.object().shape({
   password: Yup.string()
-    .min(8, "Must be at least 8 characters")
+    .min(8, "Must be atleast 8 characters")
     .max(15, "Must be less than 15 characters")
     .required("Required")
     .matches(
       /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
       "Make it More Strong"
     ),
+  confirmPassword: Yup.string()
+    .oneOf([Yup.ref("password"), null], "Password Must Match")
+    .required("Required"),
 });
 
 const ResetPasswordForm = () => {
-  const { loading, setLoading } = useContext(DataContext);
+  const { loading,setLoading } = useContext(DataContext);
 
-  const { randomString, expirationTimestamp } = useParams();
+  const{randomString , expirationTimestamp} = useParams();
   const navigate = useNavigate();
 
-  const [password, setPassword] = useState("");
-  const [errors, setErrors] = useState({});
-
-  const handleInputChange = (e) => {
-    setPassword(e.target.value);
-    setErrors({});
-  };
-
-  const handleresetPassword = async (e) => {
-    e.preventDefault();
+  const handleresetPassword = async (data) => {
     setLoading(true);
     try {
-      await Validate.validate({ password });
       let response = await AxiosService.post(
         `/student/reset-password/${randomString}/${expirationTimestamp}`,
-        { password }
+        data
       );
       if (response.status === 201) {
         toast.success("Password updated successfully", {
@@ -51,81 +45,112 @@ const ResetPasswordForm = () => {
         }, 2000);
       }
     } catch (error) {
-      console.log(error);
-      if (error.name === "ValidationError") {
-        setErrors({ password: error.errors[0] });
-      } else {
-        toast.error(
-          "Invalid token or token has expired. Please request a new reset link.",
-          {
-            position: "top-center",
-          }
-        );
-      }
+      console.log(error.response);
+      toast.error(
+        "Invalid token or token has expired.Please request a new reset link.",
+        {
+          position: "top-center",
+        }
+      );
     } finally {
       setLoading(false);
     }
   };
 
+  
+
   return (
-    <div className="resetpage">
-      <div className="row m-0">
-        <div className="col-md-8">
-          <div className="row img-container">
-            <img
-              src={logo}
-              alt="Zen Logo"
-              className="zen-logo"
-              style={{ width: "150px", height: "100px" }}
-            />
-          </div>
-          <div className="row">
-            <div className="col-md-12 d-flex flex-column justify-content-center align-items-center">
-              <div className=" col-lg-8">
-                <form onSubmit={handleresetPassword}>
-                  <div className="form-group">
-                    <label className="label-style" htmlFor="password">
-                      Password
-                    </label>
-                    <input
-                      type="password"
-                      name="password"
-                      id="password"
-                      value={password}
-                      onChange={handleInputChange}
-                      placeholder="********"
-                      className="form-control"
-                    />
-                    {errors.password && (
-                      <p style={{ color: "red" }}>{errors.password}</p>
-                    )}
-                  </div>
-                  <button
-                    style={{
-                      backgroundColor: "#4b0dba",
-                      color: "#fff",
+      <div className="resetpage">
+        <div className="row m-0">
+          <div className="col-md-8">
+            <div className="row img-container">
+              <img
+                src={logo}
+                alt="Zen Logo"
+                className="zen-logo"
+                style={{ width: "150px", height: "100px" }}
+              />
+            </div>
+            <div className="row">
+              <div className="col-md-12 d-flex flex-column justify-content-center align-items-center">
+                <div className=" col-lg-8">
+                  <Formik
+                    initialValues={{
+                      password: "",
+                      confirmPassword: "",
                     }}
-                    type="submit"
-                    className="col-12 btn btn-lg btn-block login__btn mt-4 mb-4 d-flex justify-content-center"
-                    disabled={loading}
+                    validationSchema={Validate}
+                    onSubmit={(values, { resetForm }) => {
+                      handleresetPassword(values);
+                      resetForm();
+                    }}
                   >
-                    {loading ? (
-                      <span className="spinner-border text-warning"></span>
-                    ) : (
-                      "Submit"
+                    {({ errors, touched }) => (
+                      <Form>
+                        <div className="form-group">
+                          <label className="label-style" htmlFor="password">
+                            Password
+                          </label>
+                          <Field
+                            type="password"
+                            name="password"
+                            id="password"
+                            placeholder="********"
+                            className="form-control"
+                          />
+                          {errors.password && touched.password && (
+                            <p style={{ color: "red" }}>{errors.password}</p>
+                          )}
+                        </div>
+
+                        <div className="form-group">
+                          <label
+                            className="label-style"
+                            htmlFor="confirmPassword"
+                          >
+                            Confirm Password
+                          </label>
+                          <Field
+                            type="password"
+                            name="confirmPassword"
+                            id="confirmPassword"
+                            placeholder="********"
+                            className="form-control"
+                          />
+                          {errors.confirmPassword &&
+                            touched.confirmPassword && (
+                              <p style={{ color: "red" }}>
+                                {errors.confirmPassword}
+                              </p>
+                            )}
+                        </div>
+                        <button
+                          style={{
+                            backgroundColor: "#4b0dba",
+                            color: "#fff",
+                          }}
+                          type="submit"
+                          className="col-12 btn btn-lg btn-block login__btn mt-4 mb-4 d-flex justify-content-center"
+                        >
+                          {loading ? (
+                            <span className="spinner-border text-warning"></span>
+                          ) : (
+                            "Submit"
+                          )}
+                        </button>
+                      </Form>
                     )}
-                  </button>
-                </form>
+                  </Formik>
+                </div>
               </div>
             </div>
           </div>
+          <div className="col-md-4 text-right banner-right pr-0">
+            <img src={banner} className="zenbanner" alt="Zen banner" />
+          </div>
         </div>
-        <div className="col-md-4 text-right banner-right pr-0">
-          <img src={banner} className="zenbanner" alt="Zen banner" />
-        </div>
+        <ToastContainer position="top-center" autoClose={3000} />
       </div>
-      <ToastContainer position="top-center" autoClose={3000} />
-    </div>
   );
 };
 
